c28b3fb12b757881995e406b9d6a21d4fdb838a6
Dmitrii <dimitry.vs@gmail.com>
2022-10-16 22:54:50 +0500

    fix autotest

--- models/card.js
+++ models/card.js
@@ -26,4 +26,9 @@ const cardSchema = new mongoose.Schema({
   },
 });
 
+cardSchema.path('link').validate((val) => {
+  const urlRegex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
+  return urlRegex.test(val);
+}, 'Invalid URL.');
+
 module.exports = mongoose.model('card', cardSchema);
